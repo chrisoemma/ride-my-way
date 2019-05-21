e096c2245de63c4ad145e21e5c35409b7f4b345e
chrisso emma <chrissoemma@gmail.com>
2019-05-21 10:59:08 +0300

    Add folder component: Folder home component is added which has home component

---
+++ front-end/src/components/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Home extends Component {
+    render() {
+        return (
+            <div className="home_componnent">
+                <h1>Ride-My-Way</h1>
+            </div>
+        )
+    }
+}
+
+export default Home
