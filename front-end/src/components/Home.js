d62355d31a0fa6d83f8471b0e6325df592784174
chrisso emma <chrissoemma@gmail.com>
2019-05-21 18:35:47 +0300

    Add state to check if user registered Add state to check if user is registered and flip the auth forms

--- front-end/src/components/Home.js
+++ front-end/src/components/Home.js
@@ -3,12 +3,25 @@ import Login from './auth/Login'
 import Register from './auth/Register'
 
 export class Home extends Component {
+    constructor() {
+        super()
+        this.state = {
+            isUser: true
+        }
+    }
+
+    checkRegisterUser() {
+        this.setState(prev => {
+            return {
+                isUser: !prev.isUser
+            }
+        })
+    }
     render() {
         return (
             <div className="home_componnent">
                 <h1>Ride-My-Way</h1>
-                <Login />
-                <Register />
+                {this.state.isUser ? <Login /> : <Register />}
             </div>
         )
     }
