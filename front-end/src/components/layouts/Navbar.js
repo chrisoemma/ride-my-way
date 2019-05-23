363bf158397493317e3d1623164348fd710e41ec
chrisso emma <chrissoemma@gmail.com>
2019-05-23 23:20:40 +0300

    Add default user image

--- front-end/src/components/layouts/Navbar.js
+++ front-end/src/components/layouts/Navbar.js
@@ -9,7 +9,7 @@ export class Navbar extends Component {
                         <ul>
                             <li className="brand"><a href="#">Ride-My-way</a></li>
                             <div className="pull-right">
-                                <li><a href="#">Chrisostom <img src="" /></a></li>
+                                <li><a href="#"><span> Chrisso </span><img src={require('../images/default.png')} alt="user" /></a></li>
                                 <li><a href="#"><i className="fa fa-bell badge" data-badge="5" aria-hidden="true"></i></a></li>
                             </div>
                         </ul>
