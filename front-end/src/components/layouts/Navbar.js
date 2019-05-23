820ba56ccfbb1e3b9014e1661971797cdb3c4ee8
chrisso emma <chrissoemma@gmail.com>
2019-05-23 23:47:43 +0300

    Add comments to navbar reverse the link

--- front-end/src/components/layouts/Navbar.js
+++ front-end/src/components/layouts/Navbar.js
@@ -9,6 +9,7 @@ export class Navbar extends Component {
                         <ul>
                             <li className="brand"><a href="#">Ride-My-way</a></li>
                             <div className="pull-right">
+                                {/*These two below nav are reversed due to lack of improper css styles*/}
                                 <li><a href="#"><span> Chrisso </span><img src={require('../images/default.png')} alt="user" /></a></li>
                                 <li><a href="#"><i className="fa fa-bell badge" data-badge="5" aria-hidden="true"></i></a></li>
                             </div>
