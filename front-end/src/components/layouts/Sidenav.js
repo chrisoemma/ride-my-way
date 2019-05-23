899065238e2af4d6ad70655379b2673ac6ecaaa4
chrisso emma <chrissoemma@gmail.com>
2019-05-24 02:31:01 +0300

    Add sidebar with styles

---
+++ front-end/src/components/layouts/Sidenav.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export class Sidenav extends Component {
+    render() {
+        return (
+            <div className="sidebar">
+                <a href="#home"><i className="fa fa-fw fa-home"></i>Dashbord</a>
+                <a href="#services"><i className="fa fa-fw fa-wrench"></i> Settings</a>
+            </div>
+
+        )
+    }
+}
+
+export default Sidenav
--- front-end/src/components/profile/Profile.js
+++ front-end/src/components/profile/Profile.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import Navbar from '../layouts/Navbar'
+import Sidenav from '../layouts/Sidenav'
 
 export class Profile extends Component {
     render() {
         return (
             <div>
                 <Navbar />
+                <Sidenav />
             </div>
         )
     }
