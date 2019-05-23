e312da6e29c4d98696926e0e359b49465805da2c
chrisso emma <chrissoemma@gmail.com>
2019-05-24 02:33:12 +0300

    Add button find driver: Button find driver shortcut to find driver for a passeger

--- front-end/src/components/layouts/Sidenav.js
+++ front-end/src/components/layouts/Sidenav.js
@@ -4,6 +4,7 @@ export class Sidenav extends Component {
     render() {
         return (
             <div className="sidebar">
+                <button>FIND DRIVER</button>
                 <a href="#home"><i className="fa fa-fw fa-home"></i>Dashbord</a>
                 <a href="#services"><i className="fa fa-fw fa-wrench"></i> Settings</a>
             </div>
