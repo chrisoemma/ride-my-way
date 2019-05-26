13bf739820c3d211e7a97a198e6dc6b8e5851480
chrisso emma <chrissoemma@gmail.com>
2019-05-26 15:08:25 +0300

    Change New ride component to functinal component

--- front-end/src/components/passenger/NewRide.js
+++ front-end/src/components/passenger/NewRide.js
@@ -1,24 +1,21 @@
-import React, { Component } from 'react'
+import React from 'react'
 import Navbar from '../layouts/Navbar'
 import SideNav from '../layouts/SideNav'
 import Destination from './Destination'
 import CurrentMap from './CurrentMap'
 
 
-export class NewRide extends Component {
-
-    render() {
-        return (
-            <div>
-                <Navbar />
-                <SideNav />
-                <div class="main">
-                    <CurrentMap />
-                    <Destination />
-                </div>
+function NewRide() {
+    return (
+        <div>
+            <Navbar />
+            <SideNav />
+            <div class="main">
+                <CurrentMap />
+                <Destination />
             </div>
-        )
-    }
+        </div>
+    )
 }
 
 export default NewRide
