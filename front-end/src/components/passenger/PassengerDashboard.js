6592d455d4cf238431fbe390658f35edc480ed8a
chrisso emma <chrissoemma@gmail.com>
2019-05-26 15:10:16 +0300

    Change passenger dashboard to function component

--- front-end/src/components/passenger/PassengerDashboard.js
+++ front-end/src/components/passenger/PassengerDashboard.js
@@ -1,19 +1,18 @@
-import React, { Component } from 'react'
+import React from 'react'
 import Navbar from '../layouts/Navbar'
 import SideNav from '../layouts/SideNav'
 
-export class PassengerDashboard extends Component {
-    render() {
-        return (
-            <div>
-                <Navbar />
-                <SideNav />
-                <div class="main">
+function PassengerDashboard() {
 
-                </div>
+    return (
+        <div>
+            <Navbar />
+            <SideNav />
+            <div class="main">
+
             </div>
-        )
-    }
+        </div>
+    )
 }
 
 export default PassengerDashboard
