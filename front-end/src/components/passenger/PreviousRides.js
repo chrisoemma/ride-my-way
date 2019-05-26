8da84856805b45deccdfc2ab5028f9e290504252
chrisso emma <chrissoemma@gmail.com>
2019-05-26 10:32:47 +0300

    Add table for pervious rides: Table displaying previous rides and its styles

--- front-end/src/components/passenger/PreviousRides.js
+++ front-end/src/components/passenger/PreviousRides.js
@@ -8,6 +8,34 @@ function PreviousRides() {
             <Navbar />
             <SideNav />
             <div className="main">
+                <div class="table">
+                    <table>
+                        <tr>
+                            <th>Date</th>
+                            <th>From</th>
+                            <th>Destination</th>
+                            <th>Price (Tsh)</th>
+                        </tr>
+                        <tr>
+                            <td>20/11/2018</td>
+                            <td>Posta</td>
+                            <td>Tegeta</td>
+                            <td>4000</td>
+                        </tr>
+                        <tr>
+                            <td>13/05/2015</td>
+                            <td>POSTA</td>
+                            <td>Bagamoyo</td>
+                            <td>3000</td>
+                        </tr>
+                        <tr>
+                            <td>20/02/2016</td>
+                            <td>POSTA</td>
+                            <td>TEGETA</td>
+                            <td>3000</td>
+                        </tr>
+                    </table>
+                </div>
             </div>
         </div>
 
