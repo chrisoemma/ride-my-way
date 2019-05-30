e5093fc06161a81ed8bd385380d38f357af83217
chrisso emma <chrissoemma@gmail.com>
2019-05-30 19:14:14 +0300

    Add rideOffers file that display array of rides

---
+++ back-end/rideOffers.js
@@ -0,0 +1,29 @@
+let rideOffers = [
+    {
+        id: 1,
+        passengerName: "Frank rashid",
+        from: "Magomeni",
+        to: "kigammboni",
+        phoneNo: "0734567809",
+        createdAt: "2018-12-20",
+        updatedAt: "2018-12-20"
+    },
+    {
+        id: 2,
+        passengerName: "Halid James",
+        from: "Kinondoni",
+        to: "Mabibo",
+        phoneNo: "0734567809",
+        createdAt: "2018-12-22",
+        updatedAt: "2018-12-22"
+    },
+    {
+        id: 3,
+        passengerName: "Michael kisoni",
+        from: "Pangani",
+        to: "kigoma juu",
+        phoneNo: "0734567809",
+        createdAt: "2018-11-24",
+        updatedAt: "2018-11-24"
+    }
+];
