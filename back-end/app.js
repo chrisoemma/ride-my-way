6a67ac91969535f2d5c2c315d42da679da60c11a
chrisso emma <chrissoemma@gmail.com>
2019-05-31 14:45:55 +0300

    Add error handleling script

--- back-end/app.js
+++ back-end/app.js
@@ -19,9 +19,20 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api/v1/rides', RideRouter);
 
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
+//error hundleling 
+app.use((res, req, next) => {
+  const error = new Error('Not found');
+  res.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message
+    }
+  });
 });
 
 // error handler
