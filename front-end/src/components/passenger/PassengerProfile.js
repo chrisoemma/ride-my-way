2247a76e765f2eb9f45615c2b443562af90ebaa9
chrisso emma <chrissoemma@gmail.com>
2019-06-06 22:34:43 +0300

    Add user profile info

--- front-end/src/components/passenger/PassengerProfile.js
+++ front-end/src/components/passenger/PassengerProfile.js
@@ -10,7 +10,19 @@ function PassengerProfile() {
             <Navbar />
             <SideNav />
             <div class="main">
-
+                <div class="profile_info">
+                    <div class="profile_image">
+                        <img src={require('../images/profile.jpg')} alt="user" />
+                    </div>
+                    <div class="profile_body">
+                        <h6>Passenger</h6>
+                        <h4>Chrisostom Kaweza</h4>
+                        <p>Lorem ipsum lorem ipsum
+                         loerm Lorem ipsum lorem ipsum loerm Lorem ipsum lorem ipsum loerm</p>
+                        <br />
+                        <button>update</button>
+                    </div>
+                </div>
             </div>
         </React.Fragment>
     )
