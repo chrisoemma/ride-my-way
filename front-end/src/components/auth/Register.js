e073f77e527cfa72511109ef7bb87cd54d040f41
chrisso emma <chrissoemma@gmail.com>
2019-06-07 19:42:18 +0300

    Connect register with API: Now you can register in front end and connecting with api

--- front-end/src/components/auth/Register.js
+++ front-end/src/components/auth/Register.js
@@ -1,29 +1,99 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
+import axios from 'axios'
 
-function Register(props) {
+class Register extends React.Component {
 
-    return (
 
-        <div className="register">
-            <form>
-                <label>Username</label>
-                <input type="text" placeholder="username" />
-                <label>Email</label>
-                <input type="email" placeholder="email" />
-                <label>Password</label>
-                <input type="password" placeholder="************" />
-                <label>Select Category</label>
-                <select>
-                    <option>Driver</option>
-                    <option>Passenger</option>
-                </select>
-                <button>Register</button>
-                <div className="nav-links">
-                    <li onClick={props.checkRegisterUser}>Sign In</li>
-                </div>
-            </form>
-        </div>
-    )
+    constructor() {
+        super();
+        this.state = {
+            firstName: "",
+            lastName: "",
+            email: "",
+            password: "",
+            roleId: 1
+
+        }
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(e) {
+        const { name, value } = e.target;
+        this.setState({
+            [name]: value
+        });
+    }
+
+    submit(e) {
+        e.preventDefault();
+        axios.post('/api/v1/users/register', {
+            firstName: this.state.firstName,
+            lastName: this.state.lastName,
+            email: this.state.email,
+            password: this.state.password,
+            roleId: this.state.roleId
+        }).then(res => {
+            localStorage.setItem('jwt', res.data.token);
+            this.props.history.push('/profile');
+        }
+        );
+    }
+
+    render(props) {
+        return (
+
+            <div className="register">
+                <form onSubmit={e => this.submit(e)}>
+                    <label>First Name</label>
+                    <input
+                        type="text"
+                        value={this.state.firstName}
+                        name="firstName"
+                        onChange={this.handleChange}
+                    />
+                    <label>Last Name</label>
+                    <input
+                        type="text"
+                        value={this.state.lastName}
+                        name="lastName"
+                        onChange={this.handleChange}
+                    />
+                    <label>Email</label>
+                    <input
+                        type="email"
+                        value={this.state.email}
+                        name="email"
+                        onChange={this.handleChange}
+                    />
+                    <label>Password</label>
+                    <input
+                        type="password"
+                        name="password"
+                        value={this.state.password}
+                        onChange={this.handleChange}
+                    />
+                    <label>Select Category</label>
+                    <select
+                        value={this.state.roleId}
+                        name="roleId"
+                        onChange={this.handleChange
+                        }>
+                        <option value="1">Passenger</option>
+                        <option value="2">Driver</option>
+                    </select>
+                    <button type="submit">Register</button>
+                    <div className="nav-links">
+                        <li
+                            onClick={this.props.checkRegisterUser}>
+                            Sign In
+                       </li>
+                    </div>
+                </form>
+            </div>
+        );
+    }
+
 }
 
-export default Register;
+export default withRouter(Register);
