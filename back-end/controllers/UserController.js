e3215121eea187f58e62033546261924496f36bf
chrisso emma <chrissoemma@gmail.com>
2019-06-08 03:44:54 +0300

    Add get all users api

--- back-end/controllers/UserController.js
+++ back-end/controllers/UserController.js
@@ -111,3 +111,37 @@ exports.login = (req, res, next) => {
     })
         (req, res);
 }
+
+exports.getAllUsers = (req, res, next) => {
+    User
+        .findAll()
+        .then(docs => {
+            const responce = {
+                code: 200,
+                mesaage: "Successfully",
+                count: docs.length,
+                data: {
+                    user: docs.map(user => {
+                        return {
+                            id: user.id,
+                            email: user.email,
+                            firstName: user.firstName,
+                            lastName: user.lastName,
+                            roleId: user.roleId,
+                            request: {
+                                type: "GET",
+                                url: "http://localhost:4000/users/" + user.id
+                            }
+                        }
+                    })
+                }
+
+            }
+            res.status(200).json(responce);
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: err
+            })
+        });
+}
--- back-end/routes/api/users.js
+++ back-end/routes/api/users.js
@@ -5,5 +5,6 @@ const UserController = require('../../controllers/UserController');
 
 router.post('/register', UserController.register);
 router.post('/login', UserController.login);
+router.get('/', UserController.getAllUsers);
 
 module.exports = router;
