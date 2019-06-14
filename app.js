b8646a9f7b8717858a8a744aefa3d96f052bfbcb
chrisso emma <chrissoemma@gmail.com>
2019-06-14 15:39:53 +0300

    Fix:Add s to headers

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ require('./middleware/auth');
 //prevent cors errors
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Header', 'Origin,X-Requested-With,Content-Type,Accept,Authorization');
+  res.header('Access-Control-Allow-Headers', 'Origin,X-Requested-With,Content-Type,Accept,Authorization');
   if (req.method === "OPTIONS") {
       res.header('Access-Control-Allow-Methods', 'PUT,PATCH,POST,GET,DELETE');
       return res.status(200).json({});
